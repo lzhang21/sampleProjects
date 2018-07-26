ead2a8b8466c50ffc582910a6f8e15ab5919bc1b
lzhang21 <36090548+lzhang21@users.noreply.github.com>
2018-07-26 07:23:43 -0400

    Add files via upload

---
+++ EPAAggScript.R
@@ -0,0 +1,25 @@
+#Load R-ArcGIS Bridge and other modules and set working directory 
+library(ggplot2)
+library(stringr)
+library(arcgisbinding)
+arc.check_product()
+library(sp)
+
+
+#Read in shapefile from project
+water_df <- arc.open("C:/Users/lu9739/Documents/Projects/rad_303d_20150501/rad_303d_l.shp")
+
+#Selecting only certain fields from the shapefile 
+select_df <- arc.select(object = water_df, fields = c('SOURCE_ORG', 'ELENGTH_KM'))
+
+
+impairedtable <- aggregate(select_df$ELENGTH_KM, 
+                            by = list(category = select_df$SOURCE_ORG), sum)
+
+
+
+names(impairedtable) <-c("State","KmImpairedWater")
+
+watertable <- impairedtable
+
+arc.write('C:/Users/lu9739/Documents/ArcGIS/Projects/PublicSafetyProject/PublicSafetyProject.gdb', watertable)
