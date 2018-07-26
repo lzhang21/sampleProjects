4deff06fa43888cce577bd07626ca7edcef1d051
lzhang21 <36090548+lzhang21@users.noreply.github.com>
2018-07-25 23:42:32 -0400

    Update and rename CodeJan26.R to AirportStatistics.R

--- AirportStatistics.R
+++ AirportStatistics.R
@@ -1,9 +1,16 @@
+#Sample script that reads in all Flights to/from San Francisco, filters the data, and aggregates the data into a easy to read format
+
+#First and second plot calulates passengers to and from Asia by Month (and grouped by year)
+
+# Third plot documents the yearly rise of Low Fare Airlines
+
+
 library(ggplot2)
 library(stringr)
 
-renewable_table <- read.csv(file.choose(), header = T)
 airplane_table <- read.csv(file.choose(),header = T)
-## Excute after 
+
+
 
 names(airplane_table) <- c("Time (Month)", "Airline", "AirlineCode", "Published airline",
                            "IATA code","Summary","Region","Activity","PriceCat","Terminal","Boarding_Area",
@@ -21,6 +28,9 @@ month <- str_sub(monthly_flight$Date,-2,-1)
 
 monthly_flight <- cbind(monthly_flight,year,month)
 
+
+
+# Departures to Asia (Part 1)
 ggplot(monthly_flight, aes(x = month, y = Passengers, color = factor(year), group = factor(year))) + 
   geom_line(data=subset(monthly_flight,year >= 2013))+
   labs(
@@ -30,7 +40,7 @@ ggplot(monthly_flight, aes(x = month, y = Passengers, color = factor(year), grou
     color='Year'
   ) 
 
-## Coming Back to the US
+## Coming Back to the US (Plot 2)
 
 international_flight_return <- subset(airplane_table, Summary != "Domestic" & Activity == "Deplaned" & Region == "Asia")
 monthly_flight_return <- aggregate(international_flight_return$Passengers, 
@@ -48,7 +58,7 @@ ggplot(monthly_flight_return, aes(x = month, y = Passengers, color = factor(year
   ) + scale_y_continuous(breaks = seq(0, 300000, by=30000)) 
 
 
-## Low fare airlines vs conventional 
+## Low fare airlines vs conventional (Plot 3)
 domestic_flight_cheap <- subset(airplane_table, Summary == "Domestic" & Activity == "Enplaned" & PriceCat =="Low Fare")
 domestic_flight_cheap <- aggregate(domestic_flight_cheap$Passengers, 
                             by = list(category = domestic_flight_cheap$`Time (Month)`), sum)
